b344831e105c8d7a4c457a114ce3cb3f1d7cec16
SquidCooki2 <83745822+SquidCooki2@users.noreply.github.com>
2023-03-04 13:31:45 -0500

    Comma delimiting data

--- app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
+++ app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
@@ -40,7 +40,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    //  apple > android
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
