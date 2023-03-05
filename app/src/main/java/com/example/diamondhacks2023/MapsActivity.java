7c31ac9da2908d1b57dbb5cb31596c0123b67410
SquidCooki2 <83745822+SquidCooki2@users.noreply.github.com>
2023-03-04 22:41:11 -0500

    Added city search feature

--- app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
+++ app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
@@ -3,6 +3,10 @@ package com.example.diamondhacks2023;
 import androidx.fragment.app.FragmentActivity;
 import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,6 +14,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.diamondhacks2023.databinding.ActivityMapsBinding;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -18,13 +23,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
     ArrayList<Address> votingPlaces;
+    ArrayList<String> addressData;
+    EditText search;
+    ImageView searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         votingPlaces = new ArrayList<>();
-        sortAddress();
+        addressData = new AddressData().data;
+        sortAddress("MORRISVILLE");
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -37,17 +46,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        search = findViewById(R.id.search);
+        searchButton = findViewById(R.id.search_button);
         mMap = googleMap;
 
+        updateMarkers();
+
+        searchButton.setOnClickListener(this::geoLocate);
+    }
+
+    private void geoLocate(View view) {
+        String city = search.getText().toString();
+        sortAddress(city.toUpperCase());
+        updateMarkers();
+    }
+
+    private void updateMarkers() {
+        mMap.clear();
         for (Address address : votingPlaces) {
             LatLng latLng = AddressLatLng(address.street + ", " + address.city + ", " + address.stateName + " " + address.zip);
             mMap.addMarker(new MarkerOptions().position(latLng));
@@ -69,13 +87,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     // TODO Figure out why file paths aren't working
-    public void sortAddress() {
-        AddressData addressData = new AddressData();
-        ArrayList<String> data = addressData.data;
+    public void sortAddress(String city) {
+        votingPlaces.clear();
 
-        for (int i = 0; i < data.size(); i++) {
-            String item = data.get(i);
-            if (item.lastIndexOf(",CARY,") != -1) {
+        for (int i = 0; i < addressData.size(); i++) {
+            String item = addressData.get(i);
+            if (item.lastIndexOf("," + city + ",") != -1) {
                 Scanner scanner = new Scanner(item);
                 scanner.useDelimiter(",");
 
