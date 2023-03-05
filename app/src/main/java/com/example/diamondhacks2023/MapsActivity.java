272da62c47ff6c7cd5df97d4237ebc188b1435a0
SquidCooki2 <83745822+SquidCooki2@users.noreply.github.com>
2023-03-04 19:49:37 -0500

    Removing of VotingPlaces.txt
    So here's the deal: file paths aren't working for whatever reason. For the sake of time, were just gonna go for the slightly less efficient solution in adding it to a class. Of course this will be fixed later.

--- app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
+++ app/src/main/java/com/example/diamondhacks2023/MapsActivity.java
@@ -1,9 +1,8 @@
 package com.example.diamondhacks2023;
 
 import androidx.fragment.app.FragmentActivity;
-
+import android.location.Geocoder;
 import android.os.Bundle;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,18 +10,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.diamondhacks2023.databinding.ActivityMapsBinding;
+import java.io.IOException;
 import java.util.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    ArrayList<Address> votingPlaces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        votingPlaces = new ArrayList<>();
+        sortAddress();
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -46,50 +48,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        for (Address address : votingPlaces) {
+            LatLng latLng = AddressLatLng(address.street + ", " + address.city + ", " + address.stateName + " " + address.zip);
+            mMap.addMarker(new MarkerOptions().position(latLng));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
     }
-    public void sortAddress() throws FileNotFoundException{
-        ArrayList <Address> VotingPlacesInCity = new ArrayList<Address>();
-        File places = new File("VotingPlaces.txt");
-        Scanner sc = new Scanner(places);
-        String street = "";
-        String zip = "";
-        String city = "";
-        String state = "";
-        String csvAddress = "";
-        while (sc.hasNextLine()) {
-            String fullLine = sc.nextLine();
-            if (fullLine.indexOf("WAKE") == (fullLine.indexOf(",") + 1)) {
-                int count = 0;
-                for (int i = 0; i < fullLine.length(); i++) {
-                    if (fullLine.charAt(i) == ',') {
-                        count++;
-                    }
-                    if (count == 7) {
-                        if (fullLine.lastIndexOf("RALEIGH") == (i + 1)) {
-                            int counter = 0;
-                            for (int j = 0; j < fullLine.length(); j++) {
-                                if (fullLine.charAt(j) == ',') {
-                                    counter++;
-                                }
-                                if (counter == 5) {
-                                    csvAddress = fullLine.substring(j + 1);
-                                }
-                                break;
-                            }
-                            String[] stringArray = csvAddress.split(",");
-                            street = stringArray[0] + " " + stringArray[1];
-                            zip = stringArray[4];
-                            city = stringArray[2];
-                            state = stringArray[3];
-                            Address address = new Address(street,city,state,zip);
-                            VotingPlacesInCity.add(address);
-                        }
-                    }
-                }
+
+    public LatLng AddressLatLng(String location) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<android.location.Address> addressList;
+        try {
+            addressList = geocoder.getFromLocationName(location, 1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        android.location.Address address = addressList.get(0);
+
+        return new LatLng(address.getLatitude(), address.getLongitude());
+    }
+
+    // TODO Figure out why file paths aren't working
+    public void sortAddress() {
+        AddressData addressData = new AddressData();
+        ArrayList<String> data = addressData.data;
+
+        for (int i = 0; i < data.size(); i++) {
+            String item = data.get(i);
+            if (item.lastIndexOf(",CARY,") != -1) {
+                Scanner scanner = new Scanner(item);
+                scanner.useDelimiter(",");
+
+                for (int j = 0; j < 5; j++)
+                    scanner.next();
+
+                votingPlaces.add(new Address(scanner.next() + " " + scanner.next(), scanner.next(), scanner.next(), scanner.next()));
             }
         }
     }
